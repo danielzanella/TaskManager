2662f87b63427b97c961198c772d5714ac456890
Diego giacomelli <giacomelli@gmail.com>
2016-07-19 15:30:50 -0300

    Using a msbuild to copy TaskManager.Sample files.

--- src/Task.Sample/copy.cmd
+++
@@ -1,4 +0,0 @@
-if not exist ..\..\..\TaskManager\bin\Debug\Task.Sample\ mkdir ..\..\..\TaskManager\bin\Debug\Task.Sample\
-copy /y Task.Sample.dll ..\..\..\TaskManager\bin\Debug\Task.Sample\
-copy /y Task.Sample.xml ..\..\..\TaskManager\bin\Debug\Task.Sample\
-copy /y Task.Sample.dll.config ..\..\..\TaskManager\bin\Debug\Task.Sample\
